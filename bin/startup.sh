56d9af0e51a28ad32d7f315a1db728026417150f
Jonah M. Duckles <jduckles@ou.edu>
2015-02-24 20:11:15 -0600

    simplifying behaviors with custom startup.sh script

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-exec /data/spark-1.2.1/sbin/start-master.sh
+if [ "$MODE" == "master" ]
+   then
+	exec /data/spark-1.2.1/bin/spark-class org.apache.spark.deploy.master.Master
+elif [ "$MODE" == "worker" ]
+    then
+	exec /data/spark-1.2.1/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$MASTER
+fi
