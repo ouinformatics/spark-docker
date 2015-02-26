bf47c2c4ba61d175985525509fe1cc9a78109005
Jonah M. Duckles <jduckles@ou.edu>
2015-02-26 13:27:03 -0600

    adding client case

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+WORKDIR=/data/spark-1.2.1
 if [ "$MODE" == "master" ]
    then
-	exec /data/spark-1.2.1/bin/spark-class org.apache.spark.deploy.master.Master
+	exec ${WORKDIR}/bin/spark-class org.apache.spark.deploy.master.Master
 elif [ "$MODE" == "worker" ]
     then
-	exec /data/spark-1.2.1/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$MASTER
+	exec ${WORKDIR}/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$MASTER
+elif [ "$MODE" == "client" ]
+    then 
+       exec /bin/bash # ${WORKDIR}/bin/spark-shell 
 fi
